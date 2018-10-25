743fb7f93664479a332a57bb7e2535dd7ae43531
Armedi <armedi@outlook.com>
2018-10-25 17:32:44 +0700

    user features

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,20 @@
 const router = require('express').Router()
 const Controller = require('../controller')
 
-router.get('/', (req, res) => {
-  res.send('masuk user')
-})
+router.get('/', Controller.user.dashboard)
 
 router.get('/signup', Controller.user.signUpForm)
 router.post('/signup', Controller.user.signUp)
 
 router.get('/login', Controller.user.loginForm)
 router.post('/login', Controller.user.login)
+
+router.post('/logout', Controller.user.logout)
+
+router.get('/add-balance', Controller.user.addBalanceForm)
+router.post('/add-balance', Controller.user.addBalance)
+
+router.get('/withdraw-balance', Controller.user.withdrawBalanceForm)
+router.post('/withdraw-balance', Controller.user.withdrawBalance)
 
 module.exports = router
