8b398aab7730bd0a7f4e6b56e1f489d4ee2f934d
arjunagbt <arjunagbt@gmail.com>
2018-10-25 17:34:33 +0700

    commit CRU

--- controller/project.js
+++ controller/project.js
@@ -6,34 +6,77 @@ class ProjectController {
             include: Model.User,
             order: [['id', 'ASC']]
         })
-        .then(data=>{
-            res.render('pages/projects/list', {
-                projects: data
+            .then(data => {
+                res.render('pages/projects/list', {
+                    projects: data
+                })
+                //res.send(data)
             })
-            //res.send(data)
-        })
-        .catch(err=>{
-            res.send(err)
-        })
+            .catch(err => {
+                res.send(err)
+            })
     }
-    static projectDetails(req,res){
+    static projectDetails(req, res) {
         Model.Project.findOne({
-            where:{
+            where: {
                 id: req.params.id
             },
             include: Model.User
         })
-        .then(data=>{
-            res.render('pages/projects/details', {
-                data: data
+            .then(data => {
+                res.render('pages/projects/details', {
+                    data: data,
+                    session: req.session.user
+                })
+                //res.send(data)
             })
+            .catch(err => {
+                res.send(err)
+            })
+    }
+    static addProjectGet(req, res) {
+        res.render('pages/projects/addproject')
+    }
+    static addProjectPost(req, res) {
+        if (req.session.user) {
+            Model.Project.create({
+                name: req.body.name,
+                description: req.body.description,
+                nominal_needed: req.body.nominal,
+                owner_id: req.session.user.id
+            })
+                .then(data => {
+                    res.redirect(`/projects/details/${data.id}`)
+                })
+                .catch(err=>{
+                    res.send(err)
+                })
+        } else {
+            res.send('Must Login First to Add Project')
+        }
+    }
+    static editProjectGet(req, res) {
+        Model.Project.findOne({where:{id:req.params.id}})
+        .then(data =>{
+            res.render('pages/projects/editproject', {data:data})
         })
         .catch(err=>{
             res.send(err)
         })
     }
-    static addProject(req,res){
-        res.render('pages/projects/addproject')
+    static editProjectPost(req, res) {
+        Model.Project.findOne({where:{id:req.params.id}})
+        .then(data =>{
+            data.name = req.body.name
+            data.description = req.body.description
+            return data.save()
+        })
+        .then(()=>{
+            res.redirect(`/projects/details/${req.params.id}`)
+        })
+        .catch(err=>{
+            res.send(err)
+        })
     }
 }
 
--- routes/project.js
+++ routes/project.js
@@ -3,6 +3,8 @@ const Controller = require('../controller')
 
 router.get('/', Controller.project.showProjects)
 router.get('/details/:id', Controller.project.projectDetails)
-router.get('/add', Controller.project.addProject)
-
+router.get('/add', Controller.project.addProjectGet)
+router.post('/add', Controller.project.addProjectPost)
+router.get('/edit/:id', Controller.project.editProjectGet)
+router.post('/edit/:id', Controller.project.editProjectGet)
 module.exports = router
