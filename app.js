12df09cb6bb1fb604fbd255e826b52f62e42493a
arjunagbt <arjunagbt@gmail.com>
2018-10-25 16:08:39 +0700

    crud-pending

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const express = require('express')
-const app = express()
-const routes = require('./routes')
-
-app.set('view engine', 'ejs')
-app.use('/', express.urlencoded({ extended: false }))
-app.use('/', routes)
-
-app.listen(3000, () => console.log('Running on port 3000'))
-=======
 const express = require('express')
 const app = express()
 const routes = require('./routes')
@@ -20,4 +9,3 @@ app.use('/', express.urlencoded({ extended: false }))
 app.use('/', routes)
 
 app.listen(3000, () => console.log('Running on port 3000'))
->>>>>>> ba8d5ac23ac4cbd77e1d4562805a0748ea0ac59a
